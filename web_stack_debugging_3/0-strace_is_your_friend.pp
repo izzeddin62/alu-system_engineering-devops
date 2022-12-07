9f28f1ed2ab483a77ce5480c6efac26e1b5defeb
izzeddin62 <i.ishimwe@alustudent.com>
2022-12-07 23:31:23 +0200

    add path

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # debug apache 500 server error
 
 exec { 'new-state':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path => '/usr/local/bin/:/bin/'
 }
