a0de70a8da39fd00abca5bf3ab958c35f334fd27
izzeddin62 <i.ishimwe@alustudent.com>
2022-12-08 20:41:38 +0200

    add 0-the_sky_is_the_limit_not.pp

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# debug apache 500 server error
+
+exec { 'new-state':
+    command => 'sed -i \'s/ULIMIT="-n [0-9]*"/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/'
+}
