fa2b2b43dc1369dc1f42364ece62b61ceffe0a65
izzeddin62 <i.ishimwe@alustudent.com>
2022-12-11 00:00:03 +0200

    modify pp file

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,9 @@
 # debug apache 500 server error
 
+exec {'nginx':
+    service => 'running'
+}
+
 exec { 'new-state':
     command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g" /etc/default/nginx',
     provider => 'shell',
