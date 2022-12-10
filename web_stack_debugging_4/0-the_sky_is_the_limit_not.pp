c0728ffc025cd3b5cc103764a28edc3ef32eade9
izzeddin62 <i.ishimwe@alustudent.com>
2022-12-10 23:13:06 +0200

    modify command

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # debug apache 500 server error
 
 exec { 'new-state':
-    command => 'sed -i \'s/ULIMIT=\"-n [0-9]*"/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+    command => 'sed -i "s/ULIMIT=\"-n [0-9]*\"/ULIMIT=\"-n 4096\"/g" /etc/default/nginx',
     path    => '/usr/local/bin/:/bin/',
     notify  => service['nginx']
 }
