c0f401c6c780aaf7f8024bec9fc27347532f96c8
izzeddin62 <i.ishimwe@alustudent.com>
2022-12-10 23:04:08 +0200

    restart nginx

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 # debug apache 500 server error
 
 exec { 'new-state':
-    command => 'sed -i \'s/ULIMIT="-n [0-9]*"/ULIMIT="-n 4096"/g\' /etc/default/nginx;sudo service nginx restart',
-    path    => '/usr/local/bin/:/bin/'
+    command => 'sed -i \'s/ULIMIT=\"-n [0-9]*"/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/',
+    notify  => service['nginx']
 }
